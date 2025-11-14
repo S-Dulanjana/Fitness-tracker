4ae4347b49fc91a3615f21084a83f6bb50c632d9
H.M.S.D.HERATH <sdulanjana2021@gmail.com>
2025-11-14 23:57:14 +0530

    line chart develop

--- lib/widgets/line_chart_card.dart
+++ lib/widgets/line_chart_card.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
+import 'package:fitness_tracker/constant/color.dart';
+import 'package:fitness_tracker/widgets/custom_card.dart';
+import 'package:fl_chart/fl_chart.dart';
+
 class LineChartCard extends StatelessWidget {
-  const LineChartCard({super.key});
+  LineChartCard({super.key});
+
+  //Line chart data
+  final data = LineChartData();
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return CustomCard(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Steps Overview",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: greyColor,
+            ),
+          ),
+
+          SizedBox(height: 10),
+
+          AspectRatio(
+            aspectRatio: 16 / 6,
+            child: LineChart(
+              LineChartData(
+                lineTouchData: LineTouchData(handleBuiltInTouches: true),
+                titlesData: FlTitlesData(
+                  //remove right tilte data
+                  rightTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+
+                  //remove the topside title
+                  topTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+
+                  //Bottomside title
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: (double value, TitleMeta meta) {
+                        return SideTitleWidget(child:  , meta: meta);
+                      },
+                    )
+                  )
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
